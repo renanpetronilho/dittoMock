bba38a70204d18e467e1b51b69678d112a9597e2
Renan Petronilho <renan.petronilho@platformbuilders.io>
2018-08-14 00:42:19 -0300

    ajuste para retornar  o body de acordo com que passado na chamada

--- application.go
+++ application.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 )
+
+type CallData struct {
+	Method     string
+	Timeout    int
+	StatusCode int
+	Headers    []string
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
@@ -14,15 +21,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	timeout, _ := strconv.Atoi(r.Header.Get("timeout"))
 	statusCode, _ := strconv.Atoi(r.Header.Get("statusCode"))
 
-	fmt.Println("Method -> ", method, " Timeout -> ", timeout, "statusCode ->", statusCode)
-
 	time.Sleep(time.Duration(timeout) * time.Second)
 
+	var keys []string
+	for k := range r.Header {
+		keys = append(keys, k)
+	}
+
+	calldata := CallData{Method: method, Timeout: timeout, StatusCode: statusCode, Headers: keys}
+
+	js, err := json.Marshal(calldata)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	fmt.Fprintf(w, "Path agora %s!", r.URL.Path[1:])
+	w.Write(js)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
